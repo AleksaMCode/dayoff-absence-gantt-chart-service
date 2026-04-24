26478bc89a78b3dc10cb496d7e3eee570d51dfc7
AleksaMCode <aleksamcode@gmail.com>
2026-04-24 22:03:29 +0200

    Fix log file path

--- logger/utils.py
+++ logger/utils.py
@@ -2,7 +2,7 @@ import logging
 import os
 from logging.handlers import TimedRotatingFileHandler
 
-LOG_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "logs")
+LOG_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "logs")
 os.makedirs(LOG_DIR, exist_ok=True)
 
 
